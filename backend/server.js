63fabbdc28463982845355c2cb767f857c81ab5a
hilmi <muhammadhilmi92101@gmail.com>
2022-08-22 19:01:37 +0800

    connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const dotenv =  require('dotenv');
 dotenv.config({ path: 'backend/config/config.env'})
+
+// CONNECTING TO DB
+connectDatabase();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port} in ${process.env.NODE_ENV} mode.`));
