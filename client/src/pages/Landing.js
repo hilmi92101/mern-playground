8ec319315a6a19aefb3781fc8026b4f0b56e4883
hilmi <muhammadhilmi92101@gmail.com>
2022-08-25 23:53:24 +0800

    separate styled components

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,17 +1,19 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
+// Styled Components
+import Wrapper from '../assets/wrappers/LandingPage'
 
 const Landing = () => {
-    return <main>
+    return <Wrapper>
         <nav>
             <img src={logo} alt="jobify" className="logo"/>
         </nav>
         <div className="container page">
             <div className="info">
-                <h1>job 
-                    <span>tracking</span> 
-                    app
+                <h1>Job 
+                    <span>Tracking</span> 
+                    App
                 </h1>
 
                 <p>I'm baby etsy locavore meditation big mood kale chips pickled bushwick vinyl tbh adaptogen same master cleanse lo-fi iPhone waistcoat</p>
@@ -19,10 +21,11 @@ const Landing = () => {
                 <button className="btn btn-hero">Login/Register</button>
             </div>
 
-            <img src={main} alt="job hunt" className="img" />
+            <img src={main} alt="job hunt" className="img main-img" />
 
         </div>
-    </main>
+    </Wrapper>
 }
+
 
 export default Landing
